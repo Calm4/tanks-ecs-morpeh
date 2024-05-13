026a2af0995dabea763cfce31425fb626ff7aae9
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-05-14 01:37:45 +0300

    ML_02

--- Assets/App/Scripts/ECS/Systems/HealthSystem.cs
+++ Assets/App/Scripts/ECS/Systems/HealthSystem.cs
@@ -1,4 +1,5 @@
 ﻿using App.Scripts.Components;
+using App.Scripts.ECS.Components;
 using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using UnityEngine;
@@ -9,16 +10,19 @@ namespace App.Scripts.ECS.Systems
     [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem), fileName = "Health System")]
     public sealed class HealthSystem : UpdateSystem
     {
-        private Filter _filter;
+        private Filter _playerFilter;
+        private Filter _enemiesFilter;
 
         public override void OnAwake()
         {
-            _filter = World.Filter.With<HealthComponent>().With<DamageComponent>().Build();
+            _playerFilter = World.Filter.With<PlayerComponent>().With<DamageComponent>().With<HealthComponent>().Build();
+            _playerFilter = World.Filter.With<EnemyComponent>().With<DamageComponent>().With<HealthComponent>().Build();
+            
         }
 
         public override void OnUpdate(float deltaTime)
         {
-            foreach (var entity in _filter)
+            foreach (var entity in _playerFilter)
             {
                 ref var healthComponent = ref entity.GetComponent<HealthComponent>();
                 ref var damageComponent = ref entity.GetComponent<DamageComponent>();
@@ -36,6 +40,13 @@ namespace App.Scripts.ECS.Systems
                     GameObject.Destroy(gameObjectComponent.GameObject);
                     entity.Dispose();
                 }
+            }
+
+            foreach (var entity in _enemiesFilter)
+            {
+                ref var healthComponent = ref entity.GetComponent<HealthComponent>();
+                ref var damageComponent = ref entity.GetComponent<DamageComponent>();
+                ref var gameObjectComponent = ref entity.GetComponent<GameObjectComponent>();
             }
         }
 
