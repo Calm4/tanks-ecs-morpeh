3243079e2b94fe9eef1056a8ec598c2afd98af28
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-05-15 21:57:13 +0300

    ML_10

---
+++ Assets/App/Scripts/PhysicsUpdateSystem.cs
@@ -0,0 +1,24 @@
+﻿using Scellecs.Morpeh.Systems;
+using UnityEngine;
+using Scellecs.Morpeh;
+
+namespace App.Scripts
+{
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(PhysicsUpdateSystem), fileName = "Physics Update System")]
+    public sealed class PhysicsUpdateSystem : FixedUpdateSystem
+    {
+        public override void OnAwake()
+        {
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            Simulate(deltaTime);
+        }
+
+        public static void Simulate(float dt)
+        {
+            Physics2D.Simulate(dt);
+        }
+    }
+}
